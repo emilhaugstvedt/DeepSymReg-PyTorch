54b6126353a69055172324aaf17bc02955683a5d
Emil Johannesen Haugstvedt <emiljha@stud.ntnu.no>
2022-10-03 16:50:55 +0200

    Weights not updating

--- functions.py
+++ functions.py
@@ -1,24 +1,46 @@
+from cmath import sin
 import torch
+import sympy
 
 class BaseFunction:
 
     def __init__(self) -> None:
-        self.n_input = 1
+        self.n_inputs = 1
 
 class Squared(BaseFunction):
+
+    def __init__(self) -> None:
+        super().__init__()
 
     def torch(self, x):
         return torch.square(x)
 
+    def sympy(self, x):
+        return sympy.square(x) 
+
 class Sin(BaseFunction):
+
+    def __init__(self) -> None:
+        super().__init__()
 
     def torch(self, x):
         return torch.sin(x)
 
 class Cos(BaseFunction):
 
+    def __init__(self) -> None:
+        super().__init__()
+
     def torch(self, x):
         return torch.cos(x)
+
+class Linear(BaseFunction):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def torch(self, x):
+        return x
 
 
 class Basefunction2:
@@ -28,17 +50,19 @@ class Basefunction2:
 
 class Add(Basefunction2):
 
+    def __init__(self) -> None:
+        super().__init__()
+
     def torch(self, x, y):
         return torch.add(x, y)
 
 class Multiply(Basefunction2):
 
+    def __init__(self) -> None:
+        super().__init__()
+
     def torch(self, x, y):
         return torch.multiply(x, y)
         
     
-base_functions = [  Squared.torch, 
-                    Sin.torch,
-                    Cos.torch,
-                    Add.torch,
-                    Multiply.torch ]
+base_functions = [  Squared(), Sin()]
